b09469775729891c42704a27a3b0a186edd9bc98
Seif Sheikhelarab <seifusama@gmail.com>
2026-05-09 21:50:44 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,7 @@ const port: string | number = process.env.PORT || 5726;
 // Express middleware initialization
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.set("trust proxy", true);
 
 app.use(
     cors({
